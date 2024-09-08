91bb0eebf85b8aa912066350c49ab679e910150b
Alexandra Cooper <a.cooper8789@gmail.com>
2024-09-08 20:32:55 +0100

    Change mysql config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -11,7 +11,7 @@ var (
 
 func Connect() {
 
-	d, err := gorm.Open("mysql", "user:pass(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "user:pass/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
